1340dbc33c80b0376abba588f45b5241dca296f7
Yvar2110 <ysquinones@stefanini.com>
2022-05-18 21:59:40 -0500

    tercerValidacioncompleta

--- DataSets/Bank Marketing/bank.py
+++ DataSets/Bank Marketing/bank.py
@@ -1,19 +1,29 @@
-from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.naive_bayes import GaussianNB
-from sklearn.svm import SVC
-from sklearn.tree import DecisionTreeClassifier
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from warnings import simplefilter
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.model_selection import KFold
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import f1_score
+
 
 url = 'bank-full.csv'
 
-#DATASET BANK FULL
-data = pd.read_csv(url)
-##############################################################33
+#--------------------------DATASETBANK-----------------------------------
 
-#Remplazos
+data = pd.read_csv(url)
+
+
+#Remplazo
 data.default.replace(['no','yes'], [0,1], inplace= True)
 data.job.replace(['blue-collar','management','technician','admin.','services','retired','self-employed','entrepreneur','unemployed','housemaid','student','unknown'], [0,1,2,3,4,5,6,7,8,9,10,11], inplace= True)
 data.marital.replace(['married','single','divorced'], [0,1,2], inplace= True)
@@ -24,17 +34,17 @@ data.contact.replace(['cellular','unknown','telephone'], [0,1,2], inplace= True)
 data.poutcome.replace(['unknown','failure','other','success'], [0,1,2,3], inplace= True)
 data.y.replace(['no','yes'], [0,1], inplace= True)
 
-#---------------------------------------------------------------------------------------------------
+#----------------------------------------------------------------------------
 rangos = [18,25,40,60,100]
 nombres = ['1','2','3','4',]
 data.age = pd.cut(data.age, rangos, labels=nombres)
 data.dropna(axis=0,how='any', inplace=True)
 
-#---------------------------------------------------------------------------------------------------
-#Columns innecesarys 
+#----------------------------------------------------------------------------
+#ColumnaInnecesarias
 data.drop(['balance', 'day', 'month', 'duration','campaign','pdays','previous'], axis= 1, inplace = True)
 
-#Dividir Data
+#DividirsData
 data_train = data[:30000]
 data_test = data[30000:]
 
@@ -47,153 +57,284 @@ x_test_out = np.array(data_test.drop(['y'], 1))
 y_test_out = np.array(data_test.y)
 
 
-#REGRESION LOGISTICA ----------------------------------------------------------
+# RegresionDeLogisticValidacionCruzada
 
-#MODELO
+kfold = KFold(n_splits=10)
+
+acc_scores_train_train = []
+acc_scores_test_train = []
 logreg = LogisticRegression(solver='lbfgs', max_iter = 7600)
 
-#ENTRENAMIENTO
-logreg.fit(x_train,y_train)
-
-# METRICAS
+for train, test in kfold.split(x, y):
+    logreg.fit(x[train], y[train])
+    scores_train_train = logreg.score(x[train], y[train])
+    scores_test_train = logreg.score(x[test], y[test])
+    acc_scores_train_train.append(scores_train_train)
+    acc_scores_test_train.append(scores_test_train)
+    
+y_pred = logreg.predict(x_test_out)
 
 print('*'*50)
-print('Regresión Logística')
+print('Regresión Logística Validación cruzada')
 
 # Accuracy de Entrenamiento de Entrenamiento
-print(f'accuracy de Entrenamiento de Entrenamiento: {logreg.score(x_train, y_train)}')
+print(f'accuracy de Entrenamiento de Entrenamiento: {np.array(acc_scores_train_train).mean()}')
 
 # Accuracy de Test de Entrenamiento
-print(f'accuracy de Test de Entrenamiento: {logreg.score(x_test, y_test)}')
+print(f'accuracy de Test de Entrenamiento: {np.array(acc_scores_test_train).mean()}')
 
 # Accuracy de Validación
 print(f'accuracy de Validación: {logreg.score(x_test_out, y_test_out)}')
 
 
-# VECTOR SUPPORT MACHINE
-# MODELO
-svc = SVC(gamma='auto')
+# MatrizDeConfusión
+print(f'Matriz de confusión: {confusion_matrix(y_test_out, y_pred)}')
 
-# ENTRENAMIENTO
-svc.fit(x_train, y_train)
+matriz_confusion = confusion_matrix(y_test_out, y_pred)
+plt.figure(figsize = (6, 6))
+sns.heatmap(matriz_confusion)
+plt.title("Matriz de confusion regresion logistica")
 
-# MÉTRICAS
+precision = precision_score(y_test_out, y_pred, average=None).mean()
+print(f'Precisión: {precision}')
 
-print('*'*50)
-print('Maquina de soporte vectorial')
+recall = recall_score(y_test_out, y_pred, average=None).mean()
+print(f'Re-call: {recall}')
 
-# Accuracy de Entrenamiento de Entrenamiento
-print(f'accuracy de Entrenamiento de Entrenamiento: {svc.score(x_train, y_train)}')
+f1_score = f1_score(y_test_out, y_pred, average=None).mean()
 
-# Accuracy de Test de Entrenamiento
-print(f'accuracy de Test de Entrenamiento: {svc.score(x_test, y_test)}')
+print(f'f1: {f1_score}')
 
-# Accuracy de Validación
-print(f'accuracy de Validación: {svc.score(x_test_out, y_test_out)}')
-
-# ARBOL DE DECISIÓN
-# Seleccionando un modelo
-arbol = DecisionTreeClassifier()
-# Entreno el modelo
-arbol.fit(x_train, y_train)
-
-# MÉTRICAS
-
-print('*'*50)
-print('Decisión Tree')
-
-# Accuracy de Entrenamiento de Entrenamiento
-print(f'accuracy de Entrenamiento de Entrenamiento: {arbol.score(x_train, y_train)}')
-
-# Accuracy de Test de Entrenamiento
-print(f'accuracy de Test de Entrenamiento: {arbol.score(x_test, y_test)}')
-
-# Accuracy de Validación
-print(f'accuracy de Validación: {arbol.score(x_test_out, y_test_out)}')
-
-# MAQUINA DE SOPORTE VECTORIAL-------------------------------------------------
-
-# MODELO
-svc = SVC(gamma='auto')
-
-# ENTRENAMIENTO
-svc.fit(x_train, y_train)
-
-# MÉTRICAS
-
-print('*'*50)
-print('Maquina de soporte vectorial')
-
-# Accuracy de Entrenamiento de Entrenamiento
-print(f'accuracy de Entrenamiento de Entrenamiento: {svc.score(x_train, y_train)}')
-
-# Accuracy de Test de Entrenamiento
-print(f'accuracy de Test de Entrenamiento: {svc.score(x_test, y_test)}')
-
-# Accuracy de Validación
-print(f'accuracy de Validación: {svc.score(x_test_out, y_test_out)}')
-
-
-# ARBOL DE DECISIÓN------------------------------------------------------------
-
-# Seleccionar un modelo
+#ArbolDeDecicionValidacionCruzada
 arbol = DecisionTreeClassifier()
 
-# Entreno el modelo
+# Entreno Del modelo
 arbol.fit(x_train, y_train)
+
+kfold = KFold(n_splits=10)
+
+acc_scores_train_train = []
+acc_scores_test_train = []
+# MÉTRICAS
+
+for train, test in kfold.split(x, y):
+    arbol.fit(x[train], y[train])
+    scores_train_train = arbol.score(x[train], y[train])
+    scores_test_train = arbol.score(x[test], y[test])
+    acc_scores_train_train.append(scores_train_train)
+    acc_scores_test_train.append(scores_test_train)
+    
+y_pred = arbol.predict(x_test_out)
+
 
 # MÉTRICAS
 
 print('*'*50)
-print('Decisión Tree')
+print('Arbol de decision con Validación cruzada')
 
 # Accuracy de Entrenamiento de Entrenamiento
-print(f'accuracy de Entrenamiento de Entrenamiento: {arbol.score(x_train, y_train)}')
+print(f'accuracy de Entrenamiento de Entrenamiento: {np.array(acc_scores_train_train).mean()}')
 
 # Accuracy de Test de Entrenamiento
-print(f'accuracy de Test de Entrenamiento: {arbol.score(x_test, y_test)}')
+print(f'accuracy de Test de Entrenamiento: {np.array(acc_scores_test_train).mean()}')
 
 # Accuracy de Validación
 print(f'accuracy de Validación: {arbol.score(x_test_out, y_test_out)}')
 
 
+# Matriz de confusión
+print(f'Matriz de confusión: {confusion_matrix(y_test_out, y_pred)}')
+
+matriz_confusion = confusion_matrix(y_test_out, y_pred)
+plt.figure(figsize = (6, 6))
+sns.heatmap(matriz_confusion)
+plt.title("Matriz de confusion arbol de decision")
+
+precision = precision_score(y_test_out, y_pred, average=None).mean()
+print(f'Precisión: {precision}')
+
+recall = recall_score(y_test_out, y_pred, average=None).mean()
+print(f'Re-call: {recall}')
+
+f1_score = f1_score(y_test_out, y_pred, average=None).mean()
+
+print(f'f1: {f1_score}')
 
 
 
+# RANDOMFOREST
+
+# Seleccionando el modelo
 forest = RandomForestClassifier()
 
+# Entreno el modelo
 forest.fit(x_train, y_train)
 
+# MÉTRICAS
+
+kfold = KFold(n_splits=10)
+
+acc_scores_train_train = []
+acc_scores_test_train = []
+# MÉTRICAS
+
+for train, test in kfold.split(x, y):
+    forest.fit(x[train], y[train])
+    scores_train_train = forest.score(x[train], y[train])
+    scores_test_train = forest.score(x[test], y[test])
+    acc_scores_train_train.append(scores_train_train)
+    acc_scores_test_train.append(scores_test_train)
+    
+y_pred = forest.predict(x_test_out)
+
+
+# MÉTRICAS
 
 print('*'*50)
-print('RANDOM FOREST')
+print('Random forest con Validación cruzada')
 
-print(f'accuracy de Entrenamiento de Entrenamiento: {forest.score(x_train, y_train)}')
+# Accuracy de Entrenamiento de Entrenamiento
+print(f'accuracy de Entrenamiento de Entrenamiento: {np.array(acc_scores_train_train).mean()}')
 
-
-print(f'accuracy de Test de Entrenamiento: {forest.score(x_test, y_test)}')
+# Accuracy de Test de Entrenamiento
+print(f'accuracy de Test de Entrenamiento: {np.array(acc_scores_test_train).mean()}')
 
 # Accuracy de Validación
 print(f'accuracy de Validación: {forest.score(x_test_out, y_test_out)}')
 
 
-# NAIVE BAYES
+# Matriz de confusión
+print(f'Matriz de confusión: {confusion_matrix(y_test_out, y_pred)}')
 
+matriz_confusion = confusion_matrix(y_test_out, y_pred)
+plt.figure(figsize = (6, 6))
+sns.heatmap(matriz_confusion)
+plt.title("Matriz de confusion Random forest")
+
+precision = precision_score(y_test_out, y_pred, average=None).mean()
+print(f'Precisión: {precision}')
+
+recall = recall_score(y_test_out, y_pred, average=None).mean()
+print(f'Re-call: {recall}')
+
+f1_score = f1_score(y_test_out, y_pred, average=None).mean()
+
+print(f'f1: {f1_score}')
+
+
+# NAIVE BAYES CON VALIDACION CRUZADA------------------------------------------------------------
+
+
+# Seleccionar un modelo
 nayve = GaussianNB()
 
-
+# Entreno el modelo
 nayve.fit(x_train, y_train)
 
 # MÉTRICAS
 
+kfold = KFold(n_splits=10)
+
+acc_scores_train_train = []
+acc_scores_test_train = []
+# MÉTRICAS
+
+for train, test in kfold.split(x, y):
+    nayve.fit(x[train], y[train])
+    scores_train_train = nayve.score(x[train], y[train])
+    scores_test_train = nayve.score(x[test], y[test])
+    acc_scores_train_train.append(scores_train_train)
+    acc_scores_test_train.append(scores_test_train)
+    
+y_pred = nayve.predict(x_test_out)
+
+
+# MÉTRICAS
+
 print('*'*50)
-print('NAYVE BAYES')
+print('Nayve bayes Validación cruzada')
 
 # Accuracy de Entrenamiento de Entrenamiento
-print(f'accuracy de Entrenamiento de Entrenamiento: {nayve.score(x_train, y_train)}')
+print(f'accuracy de Entrenamiento de Entrenamiento: {np.array(acc_scores_train_train).mean()}')
 
 # Accuracy de Test de Entrenamiento
-print(f'accuracy de Test de Entrenamiento: {nayve.score(x_test, y_test)}')
+print(f'accuracy de Test de Entrenamiento: {np.array(acc_scores_test_train).mean()}')
 
 # Accuracy de Validación
 print(f'accuracy de Validación: {nayve.score(x_test_out, y_test_out)}')
+
+
+# Matriz de confusión
+print(f'Matriz de confusión: {confusion_matrix(y_test_out, y_pred)}')
+
+matriz_confusion = confusion_matrix(y_test_out, y_pred)
+plt.figure(figsize = (6, 6))
+sns.heatmap(matriz_confusion)
+plt.title("Matriz de confusion Nayve")
+
+precision = precision_score(y_test_out, y_pred, average=None).mean()
+print(f'Precisión: {precision}')
+
+recall = recall_score(y_test_out, y_pred, average=None).mean()
+print(f'Re-call: {recall}')
+
+f1_score = f1_score(y_test_out, y_pred, average=None).mean()
+
+print(f'f1: {f1_score}')
+
+
+
+# MAQUINA DE SOPORTE VECTORIAL CON VALIDACION CRUZADA-------------------------------------------------
+
+# MODELO
+svc = SVC(gamma='auto')
+
+kfold = KFold(n_splits=10)
+
+acc_scores_train_train = []
+acc_scores_test_train = []
+
+# ENTRENAMIENTO
+
+for train, test in kfold.split(x, y):
+    svc.fit(x[train], y[train])
+    scores_train_train = svc.score(x[train], y[train])
+    scores_test_train = svc.score(x[test], y[test])
+    acc_scores_train_train.append(scores_train_train)
+    acc_scores_test_train.append(scores_test_train)
+    
+y_pred = svc.predict(x_test_out)
+
+
+# MÉTRICAS
+
+print('*'*50)
+print('Maquina de soporte vectorial con Validación cruzada')
+
+# Accuracy de Entrenamiento de Entrenamiento
+print(f'accuracy de Entrenamiento de Entrenamiento: {np.array(acc_scores_train_train).mean()}')
+
+# Accuracy de Test de Entrenamiento
+print(f'accuracy de Test de Entrenamiento: {np.array(acc_scores_test_train).mean()}')
+
+# Accuracy de Validación
+print(f'accuracy de Validación: {svc.score(x_test_out, y_test_out)}')
+
+
+# Matriz de confusión
+print(f'Matriz de confusión: {confusion_matrix(y_test_out, y_pred)}')
+
+matriz_confusion = confusion_matrix(y_test_out, y_pred)
+plt.figure(figsize = (6, 6))
+sns.heatmap(matriz_confusion)
+plt.title("Matriz de confusion maquina de soporte vectorial")
+
+precision = precision_score(y_test_out, y_pred, average=None).mean()
+print(f'Precisión: {precision}')
+
+recall = recall_score(y_test_out, y_pred, average=None).mean()
+print(f'Re-call: {recall}')
+
+f1_score = f1_score(y_test_out, y_pred, average=None).mean()
+
+print(f'f1: {f1_score}')
